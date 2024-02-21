069e2049609f2fb08da1618c77954e95b389953c
zltmrwdao <stefan@tmrwdao.com>
2024-02-21 14:41:17 +0800

    feat:change dao controllers path

--- src/TomorrowDAOServer.HttpApi/Controllers/DAO/DaoController.cs
+++ src/TomorrowDAOServer.HttpApi/Controllers/DAO/DaoController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using TomorrowDAOServer.DAO;
 using Volo.Abp;
 using TomorrowDAOServer.Dao.Dto;
+using TomorrowDAOServer.DAO.Dtos;
+using Volo.Abp.Application.Dtos;
 
 namespace TomorrowDAOServer.Controllers.Dao;
 
@@ -15,17 +17,41 @@ namespace TomorrowDAOServer.Controllers.Dao;
 [Route("api/app/dao")]
 public class DaoController
 {
-    private readonly IDAOAppService _daoService;
+    private readonly IDAOAppService _daoAppService;
 
-    public DaoController(IDAOAppService daoService)
+    public DaoController(IDAOAppService daoAppService)
     {
-        _daoService = daoService;
+        _daoAppService = daoAppService;
     }
 
     [HttpGet]
     [Route("contract-info")]
     public async Task<List<string>> GetContractInfoAsync(GetContractInfoInput input)
     {
-        return await _daoService.GetContractInfoAsync(input.ChainId, input.ContractAddress);
+        return await _daoAppService.GetContractInfoAsync(input.ChainId, input.ContractAddress);
+    }
+    
+    [HttpGet("dao-info")]
+    public async Task<DAOInfoDto> GetDAOByIdAsync(GetDAOInfoInput input)
+    {
+        return await _daoAppService.GetDAOByIdAsync(input);
+    }
+    
+    [HttpGet("hc-member-list")]
+    public async Task<List<string>> GetMemberListAsync(GetDAOInfoInput input)
+    {
+        return await _daoAppService.GetMemberListAsync(input);
+    }
+    
+    [HttpGet("hc-candidate-list")]
+    public async Task<List<string>> GetCandidateListAsync(GetDAOInfoInput input)
+    {
+        return await _daoAppService.GetCandidateListAsync(input);
+    }
+    
+    [HttpGet("dao-list")]
+    public async Task<PagedResultDto<DAOListDto>> GetDAOListAsync(QueryDAOListInput request)
+    {
+        return await _daoAppService.GetDAOListAsync(request);
     }
 }
