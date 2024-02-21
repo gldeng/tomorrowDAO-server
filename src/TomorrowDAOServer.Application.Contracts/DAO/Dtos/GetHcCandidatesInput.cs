59c155511f5a27cc31951ae5cbeb3a837f2470ac
devgantmrwdao <devgan@tmrwdao.com>
2024-02-21 18:40:36 +0800

    rm virtual

--- src/TomorrowDAOServer.Application.Contracts/DAO/Dtos/GetHcCandidatesInput.cs
+++ src/TomorrowDAOServer.Application.Contracts/DAO/Dtos/GetHcCandidatesInput.cs
@@ -1,12 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace TomorrowDAOServer.DAO.Dtos;
 
 public class GetHcCandidatesInput : GetDAOInfoInput
 {
-    [Range(0, int.MaxValue)]
-    public virtual int SkipCount { get; set; }
-    
-    [Range(1, int.MaxValue)]
+    public int SkipCount { get; set; }
     public int MaxResultCount { get; set; }
 }
