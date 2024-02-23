b8bc8284219bc11e6696a3c62337b2c5702ad2c0
zecong.nian <zcndao@tmrwdao.com>
2024-02-23 16:20:42 +0800

    fix: token refresh delay seconds

--- src/TomorrowDAOServer.Application.Contracts/Options/ChainOptions.cs
+++ src/TomorrowDAOServer.Application.Contracts/Options/ChainOptions.cs
@@ -7,7 +7,7 @@ public class ChainOptions
 
     public string PrivateKeyForCallTx { get; set; }= "838183d5cf676d17a3aa8daff3c70952d27285101509fcb686c74b7e9d200d62";
     public Dictionary<string, ChainInfo> ChainInfos { get; set; } = new();
-    public int TokenImageRefreshDelaySeconds { get; set; } = 3;
+    public int TokenImageRefreshDelaySeconds { get; set; } = 300;
     
     public class ChainInfo
     {
