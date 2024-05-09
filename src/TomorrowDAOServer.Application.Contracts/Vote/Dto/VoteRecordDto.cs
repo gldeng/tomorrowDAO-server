8dbf026cdbfae737096daaf6e5af2af2fa1d22e1
zltmrwdao <stefan@tmrwdao.com>
2024-05-09 15:59:51 +0800

    fix VoteRecordDto

--- src/TomorrowDAOServer.Application.Contracts/Vote/Dto/VoteRecordDto.cs
+++ src/TomorrowDAOServer.Application.Contracts/Vote/Dto/VoteRecordDto.cs
@@ -1,3 +1,4 @@
+using Google.Type;
 using TomorrowDAOServer.Enums;
 
 namespace TomorrowDAOServer.Vote.Dto;
@@ -6,7 +7,13 @@ public class VoteRecordDto
 {
     public string Voter { get; set; }
     
+    public string TransactionId { get; set; }
+    
     public int Amount { get; set; }
+    
+    public string VotingItemId {get; set; }
+    
+    public DateTime VoteTime { get; set; }
 
     public VoteOption Option { get; set; }
 }
