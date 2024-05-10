ecc908c46e61afb2afbfc1ff3c45e8d667c7a72e
devgantmrwdao <devgan@tmrwdao.com>
2024-05-10 11:23:28 +0800

    feat: run data

--- src/TomorrowDAOServer.Application/Proposal/ProposalSyncDataService.cs
+++ src/TomorrowDAOServer.Application/Proposal/ProposalSyncDataService.cs
@@ -52,6 +52,7 @@ public class ProposalSyncDataService : ScheduleSyncDataService
 
     public override async Task<long> SyncIndexerRecordsAsync(string chainId, long lastEndHeight, long newIndexHeight)
     {
+        lastEndHeight = 0;
         var skipCount = 0;
         var blockHeight = -1L;
         List<IndexerProposal> queryList;
