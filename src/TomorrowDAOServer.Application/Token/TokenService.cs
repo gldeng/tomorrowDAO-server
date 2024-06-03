ea732c9763cc753ba05d6a41cba9d399a84f935e
devgantmrwdao <devgan@tmrwdao.com>
2024-06-03 17:09:32 +0800

    feat: remove un use

--- src/TomorrowDAOServer.Application/Token/TokenService.cs
+++ src/TomorrowDAOServer.Application/Token/TokenService.cs
@@ -73,18 +73,13 @@ public class TokenService : TomorrowDAOServerAppService, ITokenService
         var tokens = list.Where(x => x.Amount > 0).Where(x => !string.IsNullOrEmpty(x.GovernanceToken))
             .Select(x => x.GovernanceToken).Distinct().ToList();
         var tokenInfoTasks = tokens.Select(x => _explorerProvider.GetTokenInfoAsync(chainId, x)).ToList();
-        var priceTasks = tokens.Select(GetPriceTask).ToList();
+        var priceTasks = tokens.Select(x => GetTokenPriceAsync(x, CommonConstant.USDT)).ToList();
         var tokenInfoResults = (await Task.WhenAll(tokenInfoTasks)).ToDictionary(x => x.Symbol, x => x); 
         var priceResults = (await Task.WhenAll(priceTasks)).ToDictionary(x => x.BaseCoin, x => x);
         var sum = list.Where(x => x.Amount > 0).Sum(x => 
             x.Amount / Math.Pow(10, Convert.ToDouble(tokenInfoResults.GetValueOrDefault(x.GovernanceToken)?.Decimals ?? "0")) 
             * (double)(priceResults.GetValueOrDefault(x.GovernanceToken)?.Price ?? 0));
         return sum;
-    }
-    
-    private Task<TokenPriceDto> GetPriceTask(string baseCoin)
-    {
-        return GetTokenPriceAsync(baseCoin, CommonConstant.USDT);
     }
 
     public async Task<TokenPriceDto> GetTokenPriceAsync(string baseCoin, string quoteCoin)
