aca8d4091ee888c240ee14c09876b724c31a5011
devgantmrwdao <devgan@tmrwdao.com>
2024-09-18 18:26:50 +0800

    feat: add log

--- src/TomorrowDAOServer.AuthServer/SignatureGrantHandler.cs
+++ src/TomorrowDAOServer.AuthServer/SignatureGrantHandler.cs
@@ -91,6 +91,7 @@ public class SignatureGrantHandler : ITokenExtensionGrant
 
                               signature:
                               """+string.Join("-", address, timestampVal);
+            _logger.LogError("newSignText:{newSignText}",newSignText);
             if (!CryptoHelper.RecoverPublicKey(signature, HashHelper.ComputeFrom(Encoding.UTF8.GetBytes(newSignText).ToHex()).ToByteArray(),
                     out var managerPublicKey))
             {
