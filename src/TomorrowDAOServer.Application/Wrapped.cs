9746165632788e9121ccc72146010677ec9ab6a3
gldeng <realgldeng@gmail.com>
2024-10-22 23:24:21 +0800

    Catch exception for more cases

--- src/TomorrowDAOServer.Application/Wrapped.cs
+++ src/TomorrowDAOServer.Application/Wrapped.cs
@@ -13,44 +13,23 @@ public class Wrapped<TEntity, TKey> : INESTRepository<TEntity, TKey> where TEnti
 {
     private readonly INESTRepository<TEntity, TKey> _inner;
 
-    public static Wrapped<TEntity, TKey> New(INESTRepository<TEntity, TKey> inner)
-    {
-        return new Wrapped<TEntity, TKey>(inner);
-    }
 
     public Wrapped(INESTRepository<TEntity, TKey> inner)
     {
         _inner = inner;
     }
 
-    public Task<TEntity> GetAsync(TKey id, string index = null)
-    {
-        return _inner.GetAsync(id, index);
-    }
-
-    public Task<TEntity> GetAsync(Func<QueryContainerDescriptor<TEntity>, QueryContainer> filterFunc = null,
-        Func<SourceFilterDescriptor<TEntity>, ISourceFilter> includeFieldFunc = null,
-        Expression<Func<TEntity, object>> sortExp = null,
-        SortOrder sortType = SortOrder.Ascending, string index = null)
-    {
-        return _inner.GetAsync(filterFunc, includeFieldFunc, sortExp, sortType, index);
-    }
-
-    public async Task<Tuple<long, List<TEntity>>> GetListAsync(
-        Func<QueryContainerDescriptor<TEntity>, QueryContainer> filterFunc = null,
-        Func<SourceFilterDescriptor<TEntity>, ISourceFilter> includeFieldFunc = null,
-        Expression<Func<TEntity, object>> sortExp = null,
-        SortOrder sortType = SortOrder.Ascending, int limit = 1000, int skip = 0, string index = null)
+    private async Task<TResult> MaybeExecute<TResult>(TResult @default, Func<Task<TResult>> func)
     {
         try
         {
-            return await _inner.GetListAsync(filterFunc, includeFieldFunc, sortExp, sortType, limit, skip, index);
+            return await func();
         }
         catch (ElasticSearchException ex)
         {
             if (ex.Message.Contains("no such index"))
             {
-                return new Tuple<long, List<TEntity>>(0, new List<TEntity>());
+                return @default;
             }
             else
             {
@@ -59,13 +38,44 @@ public class Wrapped<TEntity, TKey> : INESTRepository<TEntity, TKey> where TEnti
         }
     }
 
-    public Task<Tuple<long, List<TEntity>>> GetSortListAsync(
+    public Task<TEntity> GetAsync(TKey id, string index = null)
+    {
+        return _inner.GetAsync(id, index);
+    }
+
+    public async Task<TEntity> GetAsync(Func<QueryContainerDescriptor<TEntity>, QueryContainer> filterFunc = null,
+        Func<SourceFilterDescriptor<TEntity>, ISourceFilter> includeFieldFunc = null,
+        Expression<Func<TEntity, object>> sortExp = null,
+        SortOrder sortType = SortOrder.Ascending, string index = null)
+    {
+        return await MaybeExecute(
+            null,
+            () => _inner.GetAsync(filterFunc, includeFieldFunc, sortExp, sortType, index)
+        );
+    }
+
+    public async Task<Tuple<long, List<TEntity>>> GetListAsync(
+        Func<QueryContainerDescriptor<TEntity>, QueryContainer> filterFunc = null,
+        Func<SourceFilterDescriptor<TEntity>, ISourceFilter> includeFieldFunc = null,
+        Expression<Func<TEntity, object>> sortExp = null,
+        SortOrder sortType = SortOrder.Ascending, int limit = 1000, int skip = 0, string index = null)
+    {
+        return await MaybeExecute(
+            new Tuple<long, List<TEntity>>(0, new List<TEntity>()),
+            () => _inner.GetListAsync(filterFunc, includeFieldFunc, sortExp, sortType, limit, skip, index)
+        );
+    }
+
+    public async Task<Tuple<long, List<TEntity>>> GetSortListAsync(
         Func<QueryContainerDescriptor<TEntity>, QueryContainer> filterFunc = null,
         Func<SourceFilterDescriptor<TEntity>, ISourceFilter> includeFieldFunc = null,
         Func<SortDescriptor<TEntity>, IPromise<IList<ISort>>> sortFunc = null, int limit = 1000,
         int skip = 0, string index = null)
     {
-        return _inner.GetSortListAsync(filterFunc, includeFieldFunc, sortFunc, limit, skip, index);
+        return await MaybeExecute(
+            new Tuple<long, List<TEntity>>(0, new List<TEntity>()),
+            () => _inner.GetSortListAsync(filterFunc, includeFieldFunc, sortFunc, limit, skip, index)
+        );
     }
 
     public Task<ISearchResponse<TEntity>> SearchAsync(SearchDescriptor<TEntity> query, int skip, int size,
@@ -77,17 +87,23 @@ public class Wrapped<TEntity, TKey> : INESTRepository<TEntity, TKey> where TEnti
             highField);
     }
 
-    public Task<CountResponse> CountAsync(Func<QueryContainerDescriptor<TEntity>, QueryContainer> query,
+    public async Task<CountResponse> CountAsync(Func<QueryContainerDescriptor<TEntity>, QueryContainer> query,
         string indexPrefix = null)
     {
-        return _inner.CountAsync(query, indexPrefix);
+        return await MaybeExecute(
+            new CountResponse(),
+            () => _inner.CountAsync(query, indexPrefix)
+        );
     }
 
-    public Task<Tuple<long, List<TEntity>>> GetListByLucenceAsync(string filter,
+    public async Task<Tuple<long, List<TEntity>>> GetListByLucenceAsync(string filter,
         Func<SortDescriptor<TEntity>, IPromise<IList<ISort>>> sortFunc = null, int limit = 1000, int skip = 0,
         string index = null)
     {
-        return _inner.GetListByLucenceAsync(filter, sortFunc, limit, skip, index);
+        return await MaybeExecute(
+            new Tuple<long, List<TEntity>>(0, new List<TEntity>()),
+            () => _inner.GetListByLucenceAsync(filter, sortFunc, limit, skip, index)
+        );
     }
 
     public Task AddOrUpdateAsync(TEntity model, string index = null)
